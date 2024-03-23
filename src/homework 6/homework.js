7b71abc7d8a198890c9cb40785c8fd43ee03e4d9
Alexander Skripko <lifeisperfect3@gmail.com>
2024-03-23 20:21:51 +0300

    feat: do homework #6;

---
+++ src/homework 6/homework.js
@@ -0,0 +1,23 @@
+const diff = (a, b) => (a > b ? a - b : b - a);
+
+const isWord = (str) => /^[a-zA-Z]+$/.test(str);
+
+const pow = (a, x) => {
+  if (a === 0 && x === 0) {
+    return undefined;
+  }
+
+  if (x < 0) {
+    return undefined;
+  }
+
+  let res = 1;
+
+  for (let i = 0; i < x; i++) {
+    res *= a;
+  }
+
+  return res;
+};
+
+module.exports = { diff, isWord, pow };
